7c5c48aa5569f9e3ee0fbfc1b0819b6c1cc5a924
Nrgfrey <nickg@cloudcase.net>
2021-05-24 12:58:00 +1000

    GET check2

--- js/include.js
+++ js/include.js
@@ -4,7 +4,8 @@ $(document).ready(function () { // Call function when DOM is ready
     });
     $("div[data-includeHTML-FS]").each(function () {     
         console.log($(this).attr("data-includeHTML-FS") + "?" + "name=" + $(this).attr("data-FS-name"))
-        window.name="Bob";           
+        window.name="Bob";
+        console.log(window.name)           
         $(this).load($(this).attr("data-includeHTML-FS") + "?" + "name=" + $(this).attr("data-FS-name"));
     });
 });
