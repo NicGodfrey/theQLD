98b5a590f7e5348b8aabd753f8cbabee8dd3b139
Nrgfrey <nickg@cloudcase.net>
2021-05-24 14:42:14 +1000

    JS templates wip

--- js/include.js
+++ js/include.js
@@ -4,9 +4,12 @@ $(document).ready(function () { // Call function when DOM is ready
     });
     $("div[data-includeHTML-FS]").each(function () {     
         console.log("LOADING");
-        console.log($(this).attr("data-includeHTML-FS") + "?" + "name=" + $(this).attr("data-FS-name"));
-        window.name="Bob";
-        console.log(window.name);
-        $(this).load($(this).attr("data-includeHTML-FS") + "?" + "name=" + $(this).attr("data-FS-name"));
+        //console.log($(this).attr("data-includeHTML-FS") + "?" + "name=" + $(this).attr("data-FS-name"));
+        window.clcName=$(this).attr("data-FS-name");
+        window.clcAboutText=$(this).attr("data-FS-aboutText");
+        window.clcHelpText=$(this).attr("data-FS-helpText");
+        window.clcImgPath=$(this).attr("data-FS-imgRelPath");
+        console.log(window.clcAboutText);
+        $(this).load($(this).attr("data-includeHTML-FS"));
     });
 });
