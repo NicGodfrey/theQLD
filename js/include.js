528ab7b03ef17a1c27807e76f478cb73cad69de0
Nrgfrey <nickg@cloudcase.net>
2021-05-24 12:54:49 +1000

    GET test

--- js/include.js
+++ js/include.js
@@ -1,5 +1,10 @@
-$(document).ready(function () {
+$(document).ready(function () { // Call function when DOM is ready
     $("div[data-includeHTML]").each(function () {                
         $(this).load($(this).attr("data-includeHTML"));
+    });
+    $("div[data-includeHTML-FS]").each(function () {     
+        console.log($(this).attr("data-includeHTML-FS") + "?" + "name=" + $(this).attr("data-FS-name"))
+        window.name="Bob";           
+        $(this).load($(this).attr("data-includeHTML-FS") + "?" + "name=" + $(this).attr("data-FS-name"));
     });
 });
